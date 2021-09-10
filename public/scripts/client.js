41f6b509af39e9af54d0887c50c8625bba4ab421
SJ <sj.jega2@gmail.com>
2021-09-10 00:14:17 +0000

    Refactor code to move helper functions out of .ready

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,36 +1,38 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {  
+    
+  loadTweets();
+  
+  $("section.tweets-list").on("mouseover", function(event) {
+    const target = $(event.target).closest("article");
+    target.css({'box-shadow': '5px 5px 10px'})
+  });
 
-// const { charCountFunc } = require('./composer-char-counter');
+  $("section.tweets-list").on("mouseout", function(event) {
+    const target = $(event.target).closest("article");
+    target.css({'box-shadow': 'none'})
+  });
+  
+  $("section.tweets-list").on("mouseover", function(event) {
+    const target = event.target.localName;
+    if (target === "i") {
+      $(event.target).css({'color': 'darkgoldenrod'});
+    }
+  }),
+
+  $("section.tweets-list").on("mouseout", function(event) { 
+    const target = event.target.localName;
+    if (target === "i") {
+      $(event.target).css({'color': 'inherit'});  
+    }
+  });  
+  
+  $('.new-tweet form').submit(newTweetPost);
+
+});
 
 
-// const tweetsDataBase = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1630901014194
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1630987414194
-//   }
-// ]
+//##HELPER FUNCTIONS BELOW##\\
+
 
 const renderTweets = function(tweetsArr) {
   $('.tweets-list').empty();
@@ -45,7 +47,6 @@ const escapeXSS = function (str) {
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
-
 
 const createTweetElement = function(tweet) {  
   return (`
@@ -74,67 +75,23 @@ const loadTweets = function() {
   });
 };
 
-
-$(document).ready(function() {
-  
-  
-  // renderTweets(tweetsDataBase);
-  
-  loadTweets();
-  
-  $("section.tweets-list").on("mouseover", function(event) {
-    const target = $(event.target).closest("article");
-    target.css({'box-shadow': '5px 5px 10px'})
-  });
-
-  $("section.tweets-list").on("mouseout", function(event) {
-    const target = $(event.target).closest("article");
-    target.css({'box-shadow': 'none'})
-  });
-  
-  $("section.tweets-list").on("mouseover", function(event) {
-    const target = event.target.localName;
-    if (target === "i") {
-      $(event.target).css({'color': 'darkgoldenrod'});
-    }
-  }),
-
-  $("section.tweets-list").on("mouseout", function(event) { 
-    const target = event.target.localName;
-    if (target === "i") {
-      $(event.target).css({'color': 'inherit'});  
-    }
-  });  
-  
-  $('.new-tweet form').submit(function(event) {
-    event.preventDefault();
-    $('#empty-text').css({'display': 'none', 'visibility': 'hidden'});
-    $('#text-over-limit').css({'display': 'none', 'visibility': 'hidden'});
-    const formTextArea = $(this).find('textarea');
-    const textLength = $(formTextArea).val().length;
-    const trimmedText = $(formTextArea).val().trim();    
-    if (!trimmedText) {
-      return $('#empty-text').css({'display': 'block','visibility': 'visible', 'height': '1.5em'});
-    }    
-    if (textLength > 140) {
-      return $('#text-over-limit').css({'display': 'block', 'visibility': 'visible', 'height': '1.5em'});
-    }
-    const urlEncodedText = formTextArea.serialize();
-    $.post("/tweets", urlEncodedText, function() {
-      loadTweets();
-      $(formTextArea).val("");
-      $(".counter").text("140");
-    });    
-  });
-
-});
-
-
-
-// if ($inputField === "") {
-//   return $(".error-msgs").text("Post cannot be empty!").slideDown().show();
-// }
-
-// if ($counter.val() < 0) {
-//   return $(".error-msgs").text("Post cannot exceed over 140 character limit!").slideDown().show();
-// }
+const newTweetPost = function(event) {
+  event.preventDefault();
+  $('#empty-text').slideUp();
+  $('#text-over-limit').slideUp();
+  const formTextArea = $(this).find('textarea');
+  const textLength = $(formTextArea).val().length;
+  const trimmedText = $(formTextArea).val().trim();    
+  if (!trimmedText) {
+    return $('#empty-text').slideDown();
+  }    
+  if (textLength > 140) {
+    return $('#text-over-limit').slideDown();
+  }
+  const urlEncodedText = formTextArea.serialize();
+  $.post("/tweets", urlEncodedText, function() {
+    loadTweets();
+    $(formTextArea).val("");
+    $(".counter").text("140");
+  });    
+}
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,14 @@
+$(document).ready(function() {
+  
+  charCountFunc();
+
+});
+
+
 const charCountFunc = function() {
   $("#tweet-text").on("input", function() {
+    $('#empty-text').slideUp();
+    $('#text-over-limit').slideUp();
     let charCount = 140 - $(this).val().length;
     const counterHtmlTag = $(this).closest("form", "section#new-tweet").find("output");
     counterHtmlTag.text(charCount);
@@ -10,9 +19,3 @@ const charCountFunc = function() {
     }
   });
 }
-
-$(document).ready(function() {
-  
-  charCountFunc();
-
-});
